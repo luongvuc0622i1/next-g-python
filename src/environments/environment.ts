78e20424794dadd2a942d7fe3d2798bd88623af6
Luong <luongvh@nextgsolutions.com>
2024-01-30 10:58:35 +0700

    ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.156:8888',
+  // apiUrl: 'http://192.168.1.156:8888',
+  apiUrl: 'http://192.168.1.199:8080/Crawler_Java',
 };
